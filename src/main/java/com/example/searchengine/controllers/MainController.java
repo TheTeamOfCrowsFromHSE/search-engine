7f868a88d1d8635786680d00f1b889e917d4b804
Tsygankov-Slava <st200399200399@gmail.com>
2022-10-22 01:19:00 +0300

    init

---
+++ src/main/java/com/example/searchengine/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.example.searchengine.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Search Engine");
+        return "home";
+    }
+}
---
+++ src/test/java/com/example/searchengine/SearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
