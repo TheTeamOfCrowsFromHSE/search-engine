417b5a1367f0856ee9406170b5058f2287157b8b
Tsygankov-Slava <st200399200399@gmail.com>
2022-10-22 03:08:01 +0300

    add functionality which allows get string from SearchBox

--- src/main/java/com/example/searchengine/SearchEngineApplication.java
+++ src/main/java/com/example/searchengine/SearchEngineApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SearchEngineApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SearchEngineApplication.class, args);
     }
