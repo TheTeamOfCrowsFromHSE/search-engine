c1f66c16d0bfc0e33635a08c61bd54de983455eb
khadievedem <khadiev.edem@gmail.com>
2022-10-22 09:01:13 +0000

    fix searchRequestDivision

--- src/main/java/com/example/searchengine/SearchLogic.java
+++ src/main/java/com/example/searchengine/SearchLogic.java
@@ -3,12 +3,15 @@ package com.example.searchengine;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class SearchLogic {
     public static List<String> searchRequestDivision(String sentence) {
         List<String> dividedSentence = Arrays.asList(sentence.split(" "));
         for (int i = 0; i < dividedSentence.size(); i++) {
-            if (dividedSentence.get(i).length() < 3) dividedSentence.remove(i);
+            if (dividedSentence.get(i).length() < 3 && Pattern.matches("[0-9]+[\\\\.]?[0-9]*", dividedSentence.get(i))) {
+                dividedSentence.remove(i);
+            }
         }
         return dividedSentence;
     }
