2e67485475f2da602a3b9a5a550f7bd52627c75a
khadievedem <khadiev.edem@gmail.com>
2022-10-22 08:23:45 +0000

    add searchSampling impl

--- src/main/java/com/example/searchengine/SearchLogic.java
+++ src/main/java/com/example/searchengine/SearchLogic.java
@@ -6,10 +6,23 @@ import java.util.List;
 
 public class SearchLogic {
     public static List<String> searchRequestDivision(String sentence) {
-        List<String> dividedSentence = new ArrayList<>(Arrays.asList(sentence.split(" ")));
+        List<String> dividedSentence = Arrays.asList(sentence.split(" "));
         for (int i = 0; i < dividedSentence.size(); i++) {
             if (dividedSentence.get(i).length() < 3) dividedSentence.remove(i);
         }
         return dividedSentence;
+    }
+
+    public static List<String> searchSampling(List<String> dividedSentence) {
+        List<String> wordsSampling = new ArrayList<>();
+        for (int i = 0; i < dividedSentence.size() - 1; i++) {
+            for (int j = i + 1; j < dividedSentence.size(); j++) {
+                wordsSampling.add(dividedSentence.get(i) + " " + dividedSentence.get(j));
+            }
+        }
+        for (int i = 0; i < dividedSentence.size(); i++) {
+            wordsSampling.add(dividedSentence.get(i));
+        }
+        return wordsSampling;
     }
 }
