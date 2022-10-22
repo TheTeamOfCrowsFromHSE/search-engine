b902613b4f1318c9b64b8bbb9fd9c420eba2da79
Tsygankov-Slava <st200399200399@gmail.com>
2022-10-22 12:09:32 +0300

    added meilisearch implementation

--- src/main/java/com/example/searchengine/controllers/SearchController.java
+++ src/main/java/com/example/searchengine/controllers/SearchController.java
@@ -1,6 +1,7 @@
 package com.example.searchengine.controllers;
 
 import com.example.searchengine.SearchLogic;
+import com.example.searchengine.meilisearch.MeiliSearch;
 import com.example.searchengine.search.SearchDTO;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,7 @@ public class SearchController {
     @PostMapping("/search")
     ObjectNode newSearchDTO(@RequestBody SearchDTO newSearchDTO) {
         SearchLogic searchLogic = new SearchLogic(newSearchDTO);
-
+        MeiliSearch meiliSearch = new MeiliSearch(searchLogic.getWords());
+        return meiliSearch.getJSON();
     }
 }
