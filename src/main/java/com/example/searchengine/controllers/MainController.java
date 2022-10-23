201f0424be28528e6cde2f6935477201d2fd6ffd
Tsygankov-Slava <st200399200399@gmail.com>
2022-10-23 10:24:52 +0300

    fixed request

--- src/main/java/com/example/searchengine/controllers/MainController.java
+++ src/main/java/com/example/searchengine/controllers/MainController.java
@@ -1,11 +1,8 @@
 package com.example.searchengine.controllers;
 
-import com.example.searchengine.search.SearchDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
--- src/main/java/com/example/searchengine/controllers/SearchController.java
+++ src/main/java/com/example/searchengine/controllers/SearchController.java
@@ -3,21 +3,35 @@ package com.example.searchengine.controllers;
 import com.example.searchengine.SearchLogic;
 import com.example.searchengine.meilisearch.MeiliSearch;
 import com.example.searchengine.search.SearchDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 import java.util.List;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class SearchController {
 
-    @PostMapping("/search")
-    ResponseEntity<List<HashMap<String, Object>>> newSearchDTO(@RequestBody SearchDTO newSearchDTO)throws Exception {
-        SearchLogic searchLogic = new SearchLogic(newSearchDTO);
+//    @RequestMapping(method = RequestMethod.GET, value = "/search")
+//    public String search(@ModelAttribute RequestObject request) {
+//        return searchService.search(request);
+//    }
+
+//    @GetMapping("/search")
+//    ResponseEntity<List<HashMap<String, Object>>> newSearchDTO(@ModelAttribute @RequestBody SearchDTO newSearchDTO) throws Exception {
+//        System.out.println(newSearchDTO);
+//        SearchLogic searchLogic = new SearchLogic(newSearchDTO);
+//        MeiliSearch meiliSearch = new MeiliSearch(searchLogic.getWords());
+//        return ResponseEntity.ok(meiliSearch.json);
+//    }
+
+    @GetMapping("/search") ResponseEntity<List<HashMap<String, Object>>> newSearchDTO(@RequestParam(value = "text") String text) throws Exception {
+        SearchDTO searchDTO = new SearchDTO(text);
+        SearchLogic searchLogic = new SearchLogic(searchDTO);
         MeiliSearch meiliSearch = new MeiliSearch(searchLogic.getWords());
-        return ResponseEntity.ok(meiliSearch.getJSON());
+        System.out.println(text);
+        return ResponseEntity.ok(meiliSearch.json);
     }
 }
